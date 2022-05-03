3e9aa7657c7f4e495b40b3e2e8e4c85fe43c9b01
parkjunsoo <01junsoo@gmail.com>
2022-05-03 19:34:06 +0900

    feat: init proj

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>하이하이</Button>
+    </div>
+  );
+}
+
+export default App;
